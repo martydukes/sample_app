5eb52ee269b23fc1f31de5cb1a40b857e349a302
Marty Dukes <martydukes@gmail.com>
2013-01-04 20:00:04 -0600

    Updated the title tags to be dynamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
+
+class PagesController < ApplicationController
+
+	def home
+		@title = "Home"
+	end
+
+	def contact
+		@title = "Contact"
+	end
+
+	def about
+		@title = "About"
+	end
+end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it  "should have the right title" do
+      visit 'home'
+      page.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -13,6 +19,23 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+
+    it  "should have the right title" do
+      visit 'contact'
+      page.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+
+      it  "should have the right title" do
+      visit 'about'
+      page.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
